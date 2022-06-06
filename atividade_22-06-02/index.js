43eb7df416945ed082d948621deb339bc8cf8b0e
Victor Almeida <victorpy.1999@gmail.com>
2022-06-06 00:40:27 -0300

    created functions sync and async

---
+++ atividade_22-06-02/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+
+const conteudo = ` Estartando Devs \n Victor Almeida \n 02/06/2022`
+
+
+const writeFileAsync = (fileName, content) =>{
+    fs.writeFile(`${fileName}.txt`, content, err => {
+        if(err){
+            throw new err
+        }
+    })
+}
+
+const writeFileSync = (fileName, content) =>{
+    fs.writeFileSync(`${fileName}.txt`, content, err => {
+        if(err){
+            throw new err
+        }
+    })
+}
+
+
+for(i=1; i<= 5; i++){
+    writeFileAsync(`Async-text-${i}`, conteudo)
+    writeFileSync(`sync-text-${i}`, conteudo)
+}
+
+/* 
+Como o intuito do exercicio era usar a biblioteca "fs" e escrever aquivos no sistema,
+usei um "for" simples para chamar as funções e nomear os arquivos
+*/ 
